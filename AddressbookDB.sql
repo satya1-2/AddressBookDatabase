4355115fa10dc2acfe54b7c6d3680924ce11db4b
satya <satya143@gmail.com>
2023-05-03 16:16:44 +0530

    Added:creating table of addressbook

--- AddressbookDB.sql
+++ AddressbookDB.sql
@@ -3,5 +3,11 @@ show databases;
 create database addressbook_services;
 use addressbook_services;
 show databases;
+create table addressbook(firstName varchar(55) , 
+lastName varchar(55), address varchar(255), 
+city varchar(55), state varchar(55), 
+zip int, phoneNumber int, email varchar(55));
+show tables;
+desc addressbook;
 
 
