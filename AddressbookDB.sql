1cf91a4d590b7cd09ef294804280c76bff5799b6
satya <satya143@gmail.com>
2023-05-03 17:34:38 +0530

    Added:insert contacts into table

--- AddressbookDB.sql
+++ AddressbookDB.sql
@@ -14,3 +14,14 @@ city varchar(55), state varchar(55),
 zip int, phoneNumber int, email varchar(5));
 show tables;
 desc addressbook;
+------------------------------------------------------------- UC3 -------------------------------------------------------------------------
+Insert into addressbook(firstName,lastName,address,city,state,zip,phoneNumber,email)
+values('satya','yadav','gorakhpur','bhatparRani','uttarpradesh' ,273702,34567823,'sspra143@gmail.com'),
+('kundan','kumar','shiohar','aara','bihar',23455,23456877,'kundankumar2345@gmail.com'),
+('yadav','kumar','kothar','panipur','MH',23435,23347,'yadavnkumar2345@gmail.com');
+
+select * from addressbook;
+
+
+
+
