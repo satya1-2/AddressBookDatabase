c4b940f57babe515dcaeae85bc534b2ed1b03264
satya <satya143@gmail.com>
2023-05-03 16:06:16 +0530

    Added:creating database of addressbook

---
+++ AddressbookDB.sql
@@ -0,0 +1,7 @@
+show databases;
+------------------------------------------------------------ UC1 ----------------------------------------------------------------------
+create database addressbook_services;
+use addressbook_services;
+show databases;
+
+
